aa01c4d256950f37938f8e6cff1ee26b102e7621
0xJordan <ahmedjamlan12@gmail.com>
2023-08-07 17:31:55 +0300

    Implemented Cart and CartItem component design

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,114 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+import CartItem from './CartItem';
+
+const cartItems = [
+  {
+    id: 1,
+    image: '/img/01.png',
+    name: '215/60R17 V-523 Basco Nordico Viatti б/к шип',
+    price: '33 800 Tenge',
+    quantity: 2,
+    total: '67 600 Tenge',
+  },
+  {
+    id: 2,
+    image: '/img/01.png',
+    name: '215/60R17 V-523 Basco Nordico Viatti б/к шип',
+    price: '33 800 Tenge',
+    quantity: 2,
+    total: '67 600 Tenge',
+  },
+  {
+    id: 3,
+    image: '/img/01.png',
+    name: '215/60R17 V-523 Basco Nordico Viatti б/к шип',
+    price: '33 800 Tenge',
+    quantity: 2,
+    total: '67 600 Tenge',
+  },
+
+];
+
 function Cart() {
-  return <div>I am Cart</div>;
+  return (
+    <>
+      <div className="bg-white px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold">Cart</h1>
+        <div className="mt-8 flow-root bg-white">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <div className="flex items-center justify-end gap-6 border-[1px] border-b-0 p-3 py-5">
+                <div className="rounded-sm bg-[#F8FAFD] p-1 font-bold">
+                  Ready to order
+                  <span className="font-bold text-[#15A524]"> (5)</span>
+                </div>
+
+                <div className="font-bold text-[#566879]">
+                  Deferred <span className="font-bold text-[#E31E24]">(1)</span>
+                </div>
+
+                <button className="flex gap-3 font-bold rounded-md border-[1px] px-6 py-1"><XMarkIcon class="h-6" /> Clear</button>
+              </div>
+              <table className="min-w-full border-[1px]">
+                <thead className="border-b-[1px]">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="w-1/6 py-3.5 pl-4 pr-3 sm:pl-3"
+                    ></th>
+                    <th
+                      scope="col"
+                      className="w-1/6 px-3 py-3.5 text-left text-sm font-semibold text-[#566879]"
+                    >
+                      Designation
+                    </th>
+                    <th
+                      scope="col"
+                      className="w-1/3 px-3 py-3.5 text-left text-sm font-semibold text-[#566879]"
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-[#566879]"
+                    >
+                      Quantity
+                    </th>
+                    <th
+                      scope="col"
+                      className="w-1/6 px-3 py-3.5 text-left text-sm font-semibold text-[#566879]"
+                    >
+                      Total
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-[#566879]"
+                    ></th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  {cartItems.map((item) => (
+                    <CartItem key={item.id} item={item} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between p-4 bg-[#F8FAFD]">
+        <div className='flex gap-3'>
+          <button className="bg-white text-[#566879] border-[1px] font-bold w-64 rounded-lg ">Continue shopping</button>
+          <button className="bg-[#15A524] p-4 text-white font-bold w-64 rounded-lg">Confirm Order</button>
+        </div>
+        <div className='flex flex-col w-1/4 font-bold justify-center'>
+          <span className='font-normal text-[#566879]'>Total:</span>
+          <span className='text-2xl text-black'>988 800 Tenge</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Cart;
--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,58 @@
-function CartItem() {
+import { useState } from 'react';
+import { HeartIcon, ArchiveBoxXMarkIcon } from '@heroicons/react/24/outline';
+
+function CartItem({ item }) {
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  function addQuantity() {
+    setQuantity((prevQuantity) => prevQuantity += 1);
+  }
+
+  function subQuantity() {
+    if(quantity > 0)
+    setQuantity((prevQuantity) => prevQuantity -= 1);
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <tr key={item.email} className="even:bg-gray-50">
+      <td className="p-3">
+        <img
+          src={item.image}
+          alt={'Product'}
+          className="ml-3 w-24 rounded-md border-[1px] p-2 "
+        />
+      </td>
+      <td className="text-md px-3 py-4 font-bold">{item.name}</td>
+      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+        <div className="flex flex-col">
+          <span>Shymkent</span>
+          <span className="font-bold text-black">{item.price}</span>
+        </div>
+      </td>
+      <td className="whitespace-nowrap px-3 py-4 text-right">
+        <div className="w-fit bg-[#F8FAFD] p-0.5">
+          <button className="rounded-l-sm bg-white px-2" onClick={() => subQuantity()}>-</button>
+          <span className="px-2">{quantity}</span>
+          <button className="rounded-r-sm bg-white px-2" onClick={() => addQuantity()}>+</button>
+        </div>
+      </td>
+      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-left text-sm font-bold sm:pr-3">
+        {item.total}
+      </td>
+      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-xs font-medium sm:pr-3">
+        <div className="flex flex-col gap-1">
+          <button className="flex w-24 items-center gap-1 rounded-md bg-[#F8FAFD] p-2 text-[#566879] hover:text-black">
+            <HeartIcon className="h-6" />
+            <span>Delay</span>
+          </button>
+          <button className="flex w-24 items-center gap-1 rounded-md bg-[#F8FAFD] p-2 text-[#566879] hover:text-black">
+            <ArchiveBoxXMarkIcon className="h-6" />
+            <span>Delete</span>
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
 }
 
-export default CartItem
+export default CartItem;
