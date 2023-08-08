7bc18bf2b430d79af7c2c26da217bf9cdfb21dab
0xJordan <ahmedjamlan12@gmail.com>
2023-08-08 09:03:21 +0300

    Fixed Image source paths in Homepage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import { Link } from 'react-router-dom';
 
 const products = [
   {
@@ -100,23 +99,21 @@ function HomePage() {
               key={product.id}
               className="group relative border-[0.5px] border-slate-300 bg-white"
             >
-              <Link to={`pro/${product.id}`}>
-                <div className="absolute left-1/2 top-[80px]  hidden h-[130px] w-[140px] -translate-x-1/2 flex-col items-center justify-center rounded-lg border-4  border-double border-blue-50 bg-gray-900 bg-opacity-50 shadow-lg group-hover:flex ">
-                  <EyeIcon className="h-8 w-8  text-white" />
+              <div className="absolute left-1/2 top-[80px]  hidden h-[130px] w-[140px] -translate-x-1/2 flex-col items-center justify-center rounded-lg border-4  border-double border-blue-50 bg-gray-900 bg-opacity-50 shadow-lg group-hover:flex ">
+                <EyeIcon className="h-8 w-8  text-white" />
 
-                  <span className="text-white">Quick view</span>
-                </div>
-              </Link>
+                <span className="text-white">Quick view</span>
+              </div>
               <p className="absolute left-2 top-2 flex h-[45px] w-[45px] items-center justify-center rounded bg-[#F5F8FC]">
                 <img
-                  src="/public/img/sunny.svg"
+                  src="/img/sunny.svg"
                   alt=""
                   className="h-[25px] w-[25px] object-cover object-center"
                 />
               </p>
               <p className="absolute left-2 top-14 flex h-[45px] w-[45px] items-center justify-center rounded bg-[#F5F8FC]">
                 <img
-                  src="/public/img/winter.svg"
+                  src="/img/winter.svg"
                   alt=""
                   className="h-[25px] w-[25px] object-cover object-center"
                 />
@@ -126,7 +123,7 @@ function HomePage() {
               </a>
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-white px-7 pb-6 pt-12">
                 <img
-                  src="/public/img/01.png"
+                  src="/img/01.png"
                   alt={product.imageAlt}
                   className="mx-auto h-[80%] w-[80%] object-cover object-center"
                 />
@@ -141,15 +138,16 @@ function HomePage() {
                 <div className="mt-2 flex justify-between">
                   <div>
                     <h3 className="text-[15px] font-bold text-gray-700">
-                      {/* <a href={product.href}>
+                      <a href={product.href}>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {product.name}
-                      </a> */}
+                      </a>
                     </h3>
                     <p>
                       {' '}
                       <img
-                        src="/public/img/brand-tire.png"
+                        alt ="brand tire"
+                        src="/img/brand-tire.png"
                         className="my-3 h-auto w-[125px] object-cover object-center"
                       ></img>
                     </p>
