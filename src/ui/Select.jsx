c0dc9ffa4dd971d4074a512c074795dadb1814a3
j.bektemirov <b.o.jakhongir@gmail.com>
2023-11-22 12:18:00 +0500

    change: filter on catalog

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -29,28 +29,29 @@ export default function Select({ options, value, type, onChange, label }) {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white px-2 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {options.map((item) => (
-                  <Listbox.Option
-                    key={item.value}
-                    className={({ selected, active }) =>
-                      classNames(
-                        active || selected
-                          ? 'bg-[#F5F8FC] text-black'
-                          : 'text-[#566879]',
-                        'relative cursor-pointer select-none rounded py-3 pl-[6px] pr-5 font-normal'
-                      )
-                    }
-                    value={item.value}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center text-sm sm:text-base">
-                          <span>{item.label}</span>
-                        </div>
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
+                {options.length > 0 &&
+                  options.map((item) => (
+                    <Listbox.Option
+                      key={item.value}
+                      className={({ selected, active }) =>
+                        classNames(
+                          active || selected
+                            ? 'bg-[#F5F8FC] text-black'
+                            : 'text-[#566879]',
+                          'relative cursor-pointer select-none rounded py-3 pl-[6px] pr-5 font-normal'
+                        )
+                      }
+                      value={item.value}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center text-sm sm:text-base">
+                            <span>{item.label}</span>
+                          </div>
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
               </Listbox.Options>
             </Transition>
           </div>
